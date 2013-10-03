07458648b307f6e188bbbfeb51b297e7957781fc
Slemgrim <slemgrim@gmail.com>
2013-10-03 17:26:28 +0200

    shortcuts for debug and fullscreen (D|F)

--- src/main.js
+++ src/main.js
@@ -2,19 +2,18 @@ require.config({
     baseUrl: 'src'
 });
 
-require(["app/core", "gui/debug", "gui/canvas-handler", "game/snakepong"], function(core, Debug, CanvasHandler, Snakepong) {
+require(["app/core", "gui/canvas-handler", "game/snakepong"], function(core, CanvasHandler, Snakepong) {
 
     var $game = core.dom.el('.game'),
         $canvas = core.dom.el('#main-canvas'),
         game,
-        debug,
+        $debug = core.dom.el('#debug'),
         canvasHandler;
 
     var init = function(){
         eventBindings();
 
         game = new Snakepong($canvas);
-        debug = new Debug(core.dom.el('.debug'), core.dom.el('#debug'));
         canvasHandler = new CanvasHandler($canvas);
         canvasHandler.registerListener(game);
     }
@@ -22,6 +21,14 @@ require(["app/core", "gui/debug", "gui/canvas-handler", "game/snakepong"], funct
     var eventBindings = function(){
         var $btn = core.dom.el('.toggleFullscreen', $game);
         core.event.add($btn, 'click', toggleFullscreen);
+        core.event.add(document, 'keydown', function(evt) {
+            var key = (evt || window.evt).keyCode;
+            if(key == 68){
+                core.classlist.toggle($debug, 'show');
+            } else if(key == 70){
+                core.classlist.toggle($game, 'fullscreen');
+            }
+        });
     }
 
     var toggleFullscreen = function(){
