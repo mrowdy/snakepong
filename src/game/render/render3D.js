4766b04102994bc0b67c68db5a4bce957cae7119
Slemgrim <slemgrim@gmail.com>
2013-10-24 21:24:47 +0200

    shading and lights

--- src/game/render/render3D.js
+++ src/game/render/render3D.js
@@ -25,18 +25,32 @@ define(['app/core', 'game/render/canvas-helper', 'game/math/vector2', 'lib/cuon-
             viewMatrix = new Matrix4(),
             projMatrix = new Matrix4(),
             modelMatrix = new Matrix4(),
+            normalMatrix = new Matrix4(),
 
             vertices = new Float32Array(),
             colors = new Float32Array(),
+            normals = new Float32Array(),
             indices = new Uint8Array(),
             buffer,
             vertexBuffer,
             indexBuffer,
 
             u_MvpMatrix,
+            u_ModelMatrix,
+            u_NormalMatrix,
             a_Attribute,
             a_Position,
-            a_Color;
+            a_Normal,
+            a_Color,
+
+            u_AmbientLight,
+
+            u_LightPosition1,
+            u_LightColor1,
+            u_LightPosition2,
+            u_LightColor2,
+            u_LightPosition3,
+            u_LightColor3;
 
         var VSHADER_SOURCE = document.querySelector('#shader-vs').innerHTML,
             FSHADER_SOURCE = document.querySelector('#shader-fs').innerHTML;
@@ -46,8 +60,26 @@ define(['app/core', 'game/render/canvas-helper', 'game/math/vector2', 'lib/cuon-
             CanvasHelper.initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
 
             u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-            a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-            a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+            u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+            u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix');
+
+            u_AmbientLight = gl.getUniformLocation(gl.program, 'u_AmbientLight');
+
+            u_LightColor1 = gl.getUniformLocation(gl.program, 'u_LightColor1');
+            u_LightPosition1 = gl.getUniformLocation(gl.program, 'u_LightPosition1');
+            u_LightColor2 = gl.getUniformLocation(gl.program, 'u_LightColor2');
+            u_LightPosition2 = gl.getUniformLocation(gl.program, 'u_LightPosition2');
+            u_LightColor3 = gl.getUniformLocation(gl.program, 'u_LightColor3');
+            u_LightPosition3 = gl.getUniformLocation(gl.program, 'u_LightPosition3');
+
+            gl.uniform3f(u_LightColor1, 0.0, 0.0, 0,0);
+            gl.uniform3f(u_LightPosition1, 150.0, 100.0, 100.0);
+            gl.uniform3f(u_LightColor2, 1.0, 0.0, 0,0);
+            gl.uniform3f(u_LightPosition2, 0.0, 100.0, 1.0);
+            gl.uniform3f(u_LightColor3, 0.0, 0.0, 1,0);
+            gl.uniform3f(u_LightPosition3, 300.0, 100.0, 1.0);
+
+            gl.uniform3f(u_AmbientLight, 0.1, 0.1, 0.1);
 
             height = 100 / Math.tan(15 * (Math.PI/180));
             projMatrix.setPerspective(30, 300/200, 1, 1000);
@@ -121,8 +153,8 @@ define(['app/core', 'game/render/canvas-helper', 'game/math/vector2', 'lib/cuon-
                 1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
                 1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
                 1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
-                -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
-                -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+               -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+               -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
                 1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
             ]);
 
@@ -133,6 +165,15 @@ define(['app/core', 'game/render/canvas-helper', 'game/math/vector2', 'lib/cuon-
                 1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v1-v6-v7-v2 left
                 1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
                 1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0   // v4-v7-v6-v5 back
+            ]);
+
+            normals = new Float32Array([
+                0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
+                1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
+                0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
+               -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
+                0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
+                0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
             ]);
 
             indices = new Uint8Array([
@@ -146,8 +187,9 @@ define(['app/core', 'game/render/canvas-helper', 'game/math/vector2', 'lib/cuon-
 
             indexBuffer = gl.createBuffer();
 
+            initArrayBuffer(normals,  3, 'a_Normal');
             initArrayBuffer(vertices, 3, 'a_Position');
-            initArrayBuffer(colors, 3, 'a_Color');
+            initArrayBuffer(colors,   3, 'a_Color');
 
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
             gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
@@ -155,9 +197,27 @@ define(['app/core', 'game/render/canvas-helper', 'game/math/vector2', 'lib/cuon-
             return indices.length;
         }
 
+        var initArrayBuffer = function(data, num, attribute){
+            buffer = gl.createBuffer();
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+            a_Attribute = gl.getAttribLocation(gl.program, attribute);
+
+            gl.vertexAttribPointer(a_Attribute, num, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(a_Attribute);
+
+            return true;
+        }
+
         var drawRect = function(){
             updateMvp();
+
+            gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements);
+            gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
             gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
             gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
         };
 
@@ -174,20 +234,6 @@ define(['app/core', 'game/render/canvas-helper', 'game/math/vector2', 'lib/cuon-
 
         var updateMvp = function(){
             mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix);
-        }
-
-        var initArrayBuffer = function(data, num, attribute){
-            buffer = gl.createBuffer();
-
-            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-            gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-
-            a_Attribute = gl.getAttribLocation(gl.program, attribute);
-
-            gl.vertexAttribPointer(a_Attribute, num, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray(a_Attribute);
-
-            return true;
         }
 
         init();
